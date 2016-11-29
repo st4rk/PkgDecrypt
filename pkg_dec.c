820dfd62ffa5ae4b7a80814062a09c30cde3e0f2
st4rk <st4rkdev@gmail.com>
2016-11-29 16:39:22 +0000

    Code update and added the last pkg key that was missing

--- pkg_dec.c
+++ pkg_dec.c
@@ -24,6 +24,10 @@ unsigned char pkg_vita_3[] = {
 	0x42, 0x3A, 0xCA, 0x3A, 0x2B, 0xD5, 0x64, 0x9F, 0x96, 0x86, 0xAB, 0xAD, 0x6F, 0xD8, 0x80, 0x1F
 };
 
+unsigned char pkg_vita_4[] = {
+	0xAF, 0x07, 0xFD, 0x59, 0x65, 0x25, 0x27, 0xBA, 0xF1, 0x33, 0x89, 0x66, 0x8B, 0x17, 0xD9, 0xEA
+};
+
 typedef struct ctr {
 	unsigned char iv[AES_BLOCK_SIZE];
 	unsigned char counter[AES_BLOCK_SIZE];
@@ -36,7 +40,7 @@ int main(int argc, char **argv) {
 	char pkgName[0xFF];
 
 	memset(pkgName, 0x0, 0xFF);
-	printf("Pkg Name:\n");
+	printf("Pkg name:\n");
 	scanf("%s", pkgName);
 
 	pkg = fopen(pkgName, "rb");
@@ -74,121 +78,62 @@ int main(int argc, char **argv) {
 
 	AES_KEY key;
 
+	FILE *content = fopen("out.bin", "wb");
+
+	/**
+	 * encrypt PKG Key with AES_Key to generate the CTR Key
+	 * only with PKG Type 2, 3 and 4
+	 */
+	unsigned char ctr_key[0x10];
+
 	switch (keyType) {
-
-		/* PSP */
-		case 1: {
-			FILE *content = fopen("out.bin", "wb");
-
-			/**
-			 * Set AES CTR key and use PKG key as IV
-			 */
-
-			/* decrypt chunks */
-			unsigned char buffer[AES_BLOCK_SIZE];
-			unsigned char out[AES_BLOCK_SIZE];
-			int bytesOut = 0;
-			ctr d_ctr;	
-			memcpy(d_ctr.iv, pkg_key, AES_BLOCK_SIZE);
-			memset(d_ctr.counter, 0, AES_BLOCK_SIZE);
-			d_ctr.num = 0;
-
-			AES_set_encrypt_key(pkg_key_psp, 128, &key);
-
-			printf("Decrypting...");
-			fseek(pkg, dataOffset, SEEK_SET);
-
-			while (fread(buffer, 1, AES_BLOCK_SIZE, pkg) == AES_BLOCK_SIZE) {
-				AES_ctr128_encrypt(buffer, out, AES_BLOCK_SIZE, &key, d_ctr.iv, d_ctr.counter, &d_ctr.num);
-				fwrite(out, 1, AES_BLOCK_SIZE, content);
-			}
-
-			printf("Done !\n");
-			fclose(content);
-			fclose(pkg);
-		}
-		break;
-
-		/* Vita Type 2 */
-		case 2: {
-			FILE *content = fopen("out.bin", "wb");
-
-			/**
-			 * encrypt PKG Key with AES_Key to generate the CTR Key
-			 */
-			unsigned char ctr_key[0x10];
+		case 2:
 			AES_set_encrypt_key(pkg_vita_2, 128, &key);
 			AES_ecb_encrypt(pkg_key, ctr_key, &key, AES_ENCRYPT);
-
-			/**
-			 * Set AES CTR key and use PKG key as IV
-			 */
-
-			/* decrypt chunks */
-			unsigned char buffer[AES_BLOCK_SIZE];
-			unsigned char out[AES_BLOCK_SIZE];
-			int bytesOut = 0;
-			ctr d_ctr;	
-			memcpy(d_ctr.iv, pkg_key, AES_BLOCK_SIZE);
-			memset(d_ctr.counter, 0, AES_BLOCK_SIZE);
-			d_ctr.num = 0;
-
-			AES_set_encrypt_key(ctr_key, 128, &key);
-
-			printf("Decrypting...");
-			fseek(pkg, dataOffset, SEEK_SET);
-
-			while (fread(buffer, 1, AES_BLOCK_SIZE, pkg) == AES_BLOCK_SIZE) {
-				AES_ctr128_encrypt(buffer, out, AES_BLOCK_SIZE, &key, d_ctr.iv, d_ctr.counter, &d_ctr.num);
-				fwrite(out, 1, AES_BLOCK_SIZE, content);
-			}
-
-			printf("Done !\n");
-			fclose(content);
-			fclose(pkg);
-		}
 		break;
 
-		/* Vita Type 3 */
-		case 3: {
-			FILE *content = fopen("out.bin", "wb");
-
-			/**
-			 * encrypt PKG Key with AES_Key to generate the CTR Key
-			 */
-			unsigned char ctr_key[0x10];
+		case 3:
 			AES_set_encrypt_key(pkg_vita_3, 128, &key);
 			AES_ecb_encrypt(pkg_key, ctr_key, &key, AES_ENCRYPT);
+		break;
 
-			/**
-			 * Set AES CTR key and use PKG key as IV
-			 */
-
-			/* decrypt chunks */
-			unsigned char buffer[AES_BLOCK_SIZE];
-			unsigned char out[AES_BLOCK_SIZE];
-			int bytesOut = 0;
-			ctr d_ctr;	
-			memcpy(d_ctr.iv, pkg_key, AES_BLOCK_SIZE);
-			memset(d_ctr.counter, 0, AES_BLOCK_SIZE);
-			d_ctr.num = 0;
-
-			AES_set_encrypt_key(ctr_key, 128, &key);
-
-			printf("Decrypting...");
-			fseek(pkg, dataOffset, SEEK_SET);
-
-			while (fread(buffer, 1, AES_BLOCK_SIZE, pkg) == AES_BLOCK_SIZE) {
-				AES_ctr128_encrypt(buffer, out, AES_BLOCK_SIZE, &key, d_ctr.iv, d_ctr.counter, &d_ctr.num);
-				fwrite(out, 1, AES_BLOCK_SIZE, content);
-			}
-
-			printf("Done !\n");
-			fclose(content);
-			fclose(pkg);
-		}
+		case 4:
+			AES_set_encrypt_key(pkg_vita_4, 128, &key);
+			AES_ecb_encrypt(pkg_key, ctr_key, &key, AES_ENCRYPT);
 		break;
 	}
+
+	/**
+	 * Set AES CTR key and use PKG key as IV
+	 */
+
+	/* decrypt chunks */
+	unsigned char buffer[AES_BLOCK_SIZE];
+	unsigned char out[AES_BLOCK_SIZE];
+	int bytesOut = 0;
+	ctr d_ctr;	
+
+	memcpy(d_ctr.iv, pkg_key, AES_BLOCK_SIZE);
+	memset(d_ctr.counter, 0, AES_BLOCK_SIZE);
+
+	d_ctr.num = 0;
+
+	/**
+	 * AES CTR Decrypt, using the old key as IV
+	 */
+	AES_set_encrypt_key(keyType != 1 ? ctr_key : pkg_key_psp, 128, &key);
+
+	printf("Decrypting...");
+	fseek(pkg, dataOffset, SEEK_SET);
+
+	while (fread(buffer, 1, AES_BLOCK_SIZE, pkg) == AES_BLOCK_SIZE) {
+		AES_ctr128_encrypt(buffer, out, AES_BLOCK_SIZE, &key, d_ctr.iv, d_ctr.counter, &d_ctr.num);
+		fwrite(out, 1, AES_BLOCK_SIZE, content);
+	}
+
+	printf("Done !\n");
+	fclose(content);
+	fclose(pkg);
 
 
 	return 0;
