eeb0c7a9da839d2815eabfbfaab9a95957dee565
nekavally <weaknespase@gmail.com>
2017-10-11 21:10:48 +0300

    Fixed strings bug in sfo parser.

--- sfo.c
+++ sfo.c
@@ -9,10 +9,10 @@
 
 typedef struct
 {
-	char *name;
-	uint8_t* value;
-	uint32_t value_len;
-	uint32_t type;
+    char *name;
+    uint8_t *value;
+    uint32_t value_len;
+    uint32_t type;
 } _PSF_ITEM;
 
 typedef struct {
@@ -21,24 +21,31 @@ typedef struct {
     _PSF_ITEM *item;
 } _PSF;
 
-PSF psfParse( const uint8_t *buffer ){
+PSF psfParse( const uint8_t *buffer ) {
     uint32_t *ubuf = (uint32_t *) buffer;
-    if (*ubuf == 0x46535000 && *(ubuf+1) == 0x101){
+    if ( *ubuf == 0x46535000 && *( ubuf + 1 ) == 0x101 ) {
         char *names = (char *) buffer + *( ubuf + 2 );
         uint8_t *values = buffer + *( ubuf + 3 );
         uint32_t count = *( ubuf + 4 );
-        
+
         buffer += 20;
         _PSF *psf = malloc( sizeof( _PSF ) + sizeof( _PSF_ITEM ) * count );
         psf->item = (_PSF_ITEM *) ( (uint8_t *) psf + sizeof( _PSF ) );
 
-        for ( uint32_t i = 0; i < count;  i++){
+        for ( uint32_t i = 0; i < count; i++ ) {
             psf->item[i].name = names + *( (uint16_t *) buffer );
             psf->item[i].type = *( buffer + 3 );
-            psf->item[i].value = values + *( (uint32_t *) buffer + 3 );
             psf->item[i].value_len = *( (uint32_t *) buffer + 1 );
-			buffer += 16;
-			//printf("PSF%u: %s, %u, %X, %u\n", i, psf->item[i].name, psf->item[i].type, psf->item[i].value, psf->item[i].value_len);
+            if ( psf->item[i].type == 0x2 ) {
+                //Strings in psf isn't zero terminated
+                psf->item[i].value = malloc( psf->item[i].value_len + 1 );
+                memset( psf->item[i].value, 0, psf->item[i].value_len + 1 );
+                memcpy( psf->item[i].value, values + *( (uint32_t *) buffer + 3 ), psf->item[i].value_len );
+            } else {
+                psf->item[i].value = values + *( (uint32_t *) buffer + 3 );
+            }
+            buffer += 16;
+            //printf("PSF%u: %s, %u, %X, %u\n", i, psf->item[i].name, psf->item[i].type, psf->item[i].value, psf->item[i].value_len);
         }
 
         psf->buffer = NULL;
@@ -48,16 +55,16 @@ PSF psfParse( const uint8_t *buffer ){
     return NULL;
 }
 
-PSF psfRead( const char *path ){
+PSF psfRead( const char *path ) {
     FILE *in = fopen( path, "rb" );
-    if (in){
+    if ( in ) {
         uint8_t *buf = malloc( 0x10000 );
         int length = fread( buf, sizeof( uint8_t ), 0x10000, in );
-        if (length){
+        if ( length ) {
             PSF p = psfParse( buf );
-            if (p){
+            if ( p ) {
                 //Set buffer ptr to free it automatically later
-                ((_PSF*)p)->buffer = buf;
+                ( (_PSF *) p )->buffer = buf;
                 return p;
             } else {
                 free( buf );
@@ -69,19 +76,24 @@ PSF psfRead( const char *path ){
     return NULL;
 }
 
-void psfDiscard( PSF psf ){
+void psfDiscard( PSF psf ) {
     _PSF *_psf = (_PSF *) psf;
-    if (_psf){
-        if (_psf->buffer)
+    if ( _psf ) {
+        for ( int i = 0; i < _psf->item_count; i++ ) {
+            if ( _psf->item[i].type == 0x2 ) {
+                free( _psf->item[i].value );
+            }
+        }
+        if ( _psf->buffer )
             free( _psf->buffer );
         free( _psf );
     }
 }
 
-static int findItemIndex( _PSF* psf, const char* name){
-    if (name) {
-        for (int i = 0; i<psf->item_count; i++){
-            if (strcmp(psf->item[i].name, name) == 0){
+static int findItemIndex( _PSF *psf, const char *name ) {
+    if ( name ) {
+        for ( int i = 0; i < psf->item_count; i++ ) {
+            if ( strcmp( psf->item[i].name, name ) == 0 ) {
                 return i;
             }
         }
@@ -89,18 +101,18 @@ static int findItemIndex( _PSF* psf, const char* name){
     return -1;
 }
 
-char *psfGetString( PSF psf, const char *name ){
-    int index = findItemIndex((_PSF*) psf, name);
-    if (index >= 0){
-        return (char*) (((_PSF*)psf)->item[index].value);
+char *psfGetString( PSF psf, const char *name ) {
+    int index = findItemIndex( (_PSF *) psf, name );
+    if ( index >= 0 ) {
+        return (char *) ( ( (_PSF *) psf )->item[index].value );
     }
     return NULL;
 }
 
-int psfGetInt( PSF psf, const char *name ){
-    int index = findItemIndex((_PSF*) psf, name);
-    if (index >= 0){
-        return *((int*) (((_PSF*)psf)->item[index].value));
+int psfGetInt( PSF psf, const char *name ) {
+    int index = findItemIndex( (_PSF *) psf, name );
+    if ( index >= 0 ) {
+        return *( (int *) ( ( (_PSF *) psf )->item[index].value ) );
     }
     return 0;
 }
