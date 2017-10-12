6a808cf8c8fd3f9e2f85b1f5b87b67cbaa135707
nekavally <weaknespase@gmail.com>
2017-10-12 10:41:50 +0300

    Update 1.2.3 of pkg_dec.

--- pkg_dec.c
+++ pkg_dec.c
@@ -20,7 +20,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
-#define VERSION_PATCH 2
+#define VERSION_PATCH 3
 
 int decode_license( char *encoded, uint8_t *target ) {
     //First check encoded buffer
